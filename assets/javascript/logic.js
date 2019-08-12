925ebef3d6859b500b36725bdbcb270c6bfdfc33
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-08-12 14:02:04 -0500

    initial html done and firebase added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAwC-iZdMk3XUpL0JMve3-g53XCWnht5EI",
+    authDomain: "btgoligowski24-trainactivity.firebaseapp.com",
+    databaseURL: "https://btgoligowski24-trainactivity.firebaseio.com",
+    projectId: "btgoligowski24-trainactivity",
+    storageBucket: "",
+    messagingSenderId: "548245298045",
+    appId: "1:548245298045:web:bf8031f14ffb168a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
